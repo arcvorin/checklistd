05e06e2d1b0faca1a3a7c9326347f0de95799dfe
Arc Vorin <arcvorin@gmail.com>
2026-07-12 16:05:56 -0400

    Initial Commit

---
+++ Checklistd/Checklistd/ChecklistdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistdApp.swift
+//  Checklistd
+//
+//  Created by Javier Matusevich on 2026-07-12.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Checklistd/Checklistd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Checklistd
+//
+//  Created by Javier Matusevich on 2026-07-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
